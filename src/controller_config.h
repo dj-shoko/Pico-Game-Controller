bac1a04b6255d32afc4d199198a7b20b2ba62e81
Raymond Quach <56996265+dj-shoko@users.noreply.github.com>
2024-03-02 05:44:49 -0800

    Update controller_config.h
    Updated for Axion-Board

--- src/controller_config.h
+++ src/controller_config.h
@@ -2,9 +2,9 @@
 #define CONTROLLER_CONFIG_H
 
 #define SW_GPIO_SIZE 11               // Number of switches
-#define LED_GPIO_SIZE 10              // Number of switch LEDs
+#define LED_GPIO_SIZE 11              // Number of switch LEDs
 #define ENC_GPIO_SIZE 2               // Number of encoders
-#define ENC_PPR 600                   // Encoder PPR
+#define ENC_PPR 72                   // Encoder PPR
 #define MOUSE_SENS 1                  // Mouse sensitivity multiplier
 #define ENC_DEBOUNCE false            // Encoder Debouncing
 #define SW_DEBOUNCE_TIME_US 4000      // Switch debounce delay in us
@@ -22,14 +22,14 @@ const uint8_t SW_KEYCODE[] = {HID_KEY_D, HID_KEY_F, HID_KEY_J, HID_KEY_K,
                               HID_KEY_C, HID_KEY_M, HID_KEY_A, HID_KEY_B,
                               HID_KEY_1, HID_KEY_E, HID_KEY_G};
 const uint8_t SW_GPIO[] = {
-    4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 27,
+    18, 16, 14, 12, 10, 8, 6, 4, 2, 22, 24
 };
 const uint8_t LED_GPIO[] = {
-    5, 7, 9, 11, 13, 15, 17, 19, 21, 26,
+    19, 17, 15, 13, 11, 9, 7, 5, 3, 23, 25
 };
-const uint8_t ENC_GPIO[] = {0, 2};      // L_ENC(0, 1); R_ENC(2, 3)
+const uint8_t ENC_GPIO[] = {26, 28};      // L_ENC(0, 1); R_ENC(2, 3)
 const bool ENC_REV[] = {false, false};  // Reverse Encoders
-const uint8_t WS2812B_GPIO = 28;
+const uint8_t WS2812B_GPIO = 0;
 
 #endif
 
